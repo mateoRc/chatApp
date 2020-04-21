0853e46d8a71199cebd883e18372360477e0d354
mateoLaptop <mmateo19992@gmail.com>
2020-04-21 19:14:16 +0200

    setting up

---
+++ index.js
@@ -0,0 +1,7 @@
+var test = require('express');
+
+if(test) {
+    console.log('its thasderee');
+} else {
+    console.log('its not');
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+var test = require('express');
+
+if(test) {
+    console.log('its theree');
+} else {
+    console.log('its not');
+}
