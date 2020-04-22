23972711aadf7aa0bdfb99077b166494503f789e
mateoLaptop <mmateo19992@gmail.com>
2020-04-22 19:33:18 +0200

    login page

--- server.js
+++ server.js
@@ -3,12 +3,27 @@ const http = require('http');
 
 const app = express();
 const server = http.Server(app);
+// socket.io needs an http server to send over a socket 
+const io = require('socket.io')(server);
 
 server.listen(3333, () => {
     console.log('Dev server is running at port 3333');
 });
 
-// Handle the http GET request which prompts us to home dir, respond with hello
+// Handle the http GET request which prompts us to HOME ('/') dir, respond with file index.html
 app.get('/', (req, res) => {
-   res.send('hello'); 
+   //res.send('hello');
+   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/styles/index.css', (req, res) => {
+    res.sendFile(`${__dirname}/styles/index.css`);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user has connected!;');
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected!');
+    });
 });
