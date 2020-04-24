bf7cf488846b1db1dd798256232af5338958b97f
mateoLaptop <mmateo19992@gmail.com>
2020-04-24 21:14:38 +0200

    hg

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const http = require('http');
 
 const app = express();
 const server = http.Server(app);
+
 // socket.io needs an http server to send over a socket 
 const io = require('socket.io')(server);
+
+const users = [];
 
 server.listen(3333, () => {
     console.log('Dev server is running at port 3333');
@@ -12,7 +15,8 @@ server.listen(3333, () => {
 
 // Handle the http GET request which prompts us to HOME ('/') dir, respond with file index.html
 app.get('/', (req, res) => {
-   //res.send('hello');
+   
+    //res.send('hello');
    res.sendFile(`${__dirname}/index.html`);
 });
 
@@ -21,9 +25,20 @@ app.get('/styles/index.css', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user has connected!;');
+    let name = '';
+    // test
+    // console.log('A user has connected!;');
+     socket.on('has connected', (username) => {
+        name = username;
+        users.push(username);
+        
+        // emit to all sockets
+        io.emit('has connected', {username: username, userList: users});
+    });
 
     socket.on('disconnect', () => {
+        users.splice(users.indexOf(name), 1);
         console.log('A user has disconnected!');
+        io.emit('has disconnected', {username: name, userList: users})
     });
 });
