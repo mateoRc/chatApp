cdf15b665eee77de5108c896b742df7f0c528e35
mateoLaptop <mmateo19992@gmail.com>
2020-04-25 12:38:54 +0200

    final

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ server.listen(3333, () => {
 // Handle the http GET request which prompts us to HOME ('/') dir, respond with file index.html
 app.get('/', (req, res) => {
    
-    //res.send('hello');
-   res.sendFile(`${__dirname}/index.html`);
+    // test
+    // res.send('hello');
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get('/styles/index.css', (req, res) => {
@@ -26,13 +27,14 @@ app.get('/styles/index.css', (req, res) => {
 
 io.on('connection', (socket) => {
     let name = '';
+    
     // test
     // console.log('A user has connected!;');
      socket.on('has connected', (username) => {
         name = username;
         users.push(username);
         
-        // emit to all sockets
+        // emit to ALL sockets
         io.emit('has connected', {username: username, userList: users});
     });
 
@@ -41,4 +43,11 @@ io.on('connection', (socket) => {
         console.log('A user has disconnected!');
         io.emit('has disconnected', {username: name, userList: users})
     });
+
+    // handle the submit message event
+    socket.on('new message', (messageData) => {
+        console.log(messageData);
+        io.emit('new message', {username: messageData.username, message: messageData.message});
+    });
+
 });
